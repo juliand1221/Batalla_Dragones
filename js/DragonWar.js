5c9559161cce869d59da84edb32c13533efe2d2d
Julian David Tenorio <judt1221@hotmail.com>
2023-05-24 00:18:47 -0500

    Arreglo de CSS

--- js/DragonWar.js
+++ js/DragonWar.js
@@ -36,7 +36,7 @@ function seleccionarDragonJugador(){
 
 
     let selectionAtaque = document.getElementById('selec-ataque')
-    selectionAtaque.style.display = 'block'
+    selectionAtaque.style.display = 'flex'
 
 
     let inputFlareon = document.getElementById('flareon')
@@ -47,26 +47,46 @@ function seleccionarDragonJugador(){
     let inputGaia = document.getElementById('gaia')
     let spanDragonJugador = document.getElementById('dragon-jugador')
 
+    let imagenDragonJugador = document.getElementById('imagenDragonJugador')
+    let dragonUno = document.createElement('img');
+    let dragonDos = document.createElement('img');
+    let dragonTres = document.createElement('img');
+    let dragonCuatro = document.createElement('img');
+    let dragonCinco = document.createElement('img');
+    let dragonSeis = document.createElement('img');
 
+    dragonUno.src= '../images/dragonFire.jpg'
+    dragonDos.src= '../images/dragonWater.jpg'
+    dragonTres.src= '../images/dragonEarth.jpg'
+    dragonCuatro.src= '../images/dragonElectry.jpg'
+    dragonCinco.src= '../images/dragonFuegoElectric.jpg'
+    dragonSeis.src= '../images/dragonAguaTierra.jpg'
+    
 
     if(inputFlareon.checked){
         alert('Seleccionaste a Flareon')
         spanDragonJugador.innerHTML = 'Flareon'
+        imagenDragonJugador.appendChild(dragonUno)
     }else if(inputMizuchi.checked){
         alert('Seleccionaste a Mizuchi')
         spanDragonJugador.innerHTML = 'Mizuchi'
+        imagenDragonJugador.appendChild(dragonDos)
     }else if(inputKrakatoa.checked){
         alert('Seleccionaste a Krakatoa')
         spanDragonJugador.innerHTML = 'Krakatoa'
+        imagenDragonJugador.appendChild(dragonTres)
     }else if(inputFulgor.checked){
         alert('Seleccionaste a Fulgor')
         spanDragonJugador.innerHTML = 'Fulgor'
+        imagenDragonJugador.appendChild(dragonCuatro)
     }else if(inputPyrothor.checked){
         alert('Seleccionaste a Pyrothor')
         spanDragonJugador.innerHTML = 'Pyrothor'
+        imagenDragonJugador.appendChild(dragonCinco)
     }else if(inputGaia.checked){
         alert('Seleccionaste a Gaia')
         spanDragonJugador.innerHTML = 'Gaia'
+        imagenDragonJugador.appendChild(dragonSeis)
     }else {
         alert('Selecciona un Dragon')
     }
@@ -78,69 +98,92 @@ function seleccionarDragonJugador(){
 function seleccionarDragonRival(){
     let dragonAleatorio = aleatorio(1,6);
     let spanDragonRival = document.getElementById('dragon-enemigo');
+    let imagenDragonRival = document.getElementById('imagenDragonEnemigo')
+    let dragonUno = document.createElement('img');
+    let dragonDos = document.createElement('img');
+    let dragonTres = document.createElement('img');
+    let dragonCuatro = document.createElement('img');
+    let dragonCinco = document.createElement('img');
+    let dragonSeis = document.createElement('img');
+
+    dragonUno.src= '../images/dragonFire.jpg'
+    dragonDos.src= '../images/dragonWater.jpg'
+    dragonTres.src= '../images/dragonEarth.jpg'
+    dragonCuatro.src= '../images/dragonElectry.jpg'
+    dragonCinco.src= '../images/dragonFuegoElectric.jpg'
+    dragonSeis.src= '../images/dragonAguaTierra.jpg'
+
 
     if(dragonAleatorio == 1){
         alert('Tu oponente escogio Flareon')
         spanDragonRival.innerHTML = 'Flareon'
+        imagenDragonRival.appendChild(dragonUno)
     }else if(dragonAleatorio == 2){
         alert('Tu oponente escogio Mizuchi')
         spanDragonRival.innerHTML = 'Mizuchi'
+        imagenDragonRival.appendChild(dragonDos)
     }else if(dragonAleatorio == 3){
         alert('Tu oponente escogio Krakatoa')
         spanDragonRival.innerHTML = 'Krakatoa'
+        imagenDragonRival.appendChild(dragonTres)
     }else if(dragonAleatorio == 4){
         alert('Tu oponente escogio Fulgor')
         spanDragonRival.innerHTML = 'Fulgor'
+        imagenDragonRival.appendChild(dragonCuatro)
     }else if(dragonAleatorio == 5){
         alert('Tu oponente escogio Pyrothor')
         spanDragonRival.innerHTML = 'Pyrothor'
+        imagenDragonRival.appendChild(dragonCinco)
     }else if(dragonAleatorio == 6){
         alert('Tu oponente escogio Gaia')
         spanDragonRival.innerHTML = 'Gaia'
+        imagenDragonRival.appendChild(dragonSeis)
     }
+
 }
 
 function ataqueFuego() {
-    ataqueJugador = 'FUEGO'
+    ataqueJugador = 'ALIENTO DE FUEGO'
     alert('Tu Dragon lanza '+ ataqueJugador +' a su oponente')
     seleccionarAtaqueRival();
 }
 function ataqueAgua() {
-    ataqueJugador = 'AGUA'
+    ataqueJugador = 'ALIENTO DE HIELO'
     alert('Tu Dragon lanza '+ ataqueJugador +' a su oponente')
     seleccionarAtaqueRival();
 }
 function ataqueTierra() {
-    ataqueJugador = 'TIERRA'
+    ataqueJugador = 'RAFAGA DE VIENTO'
     alert('Tu Dragon lanza '+ ataqueJugador +' a su oponente')
     seleccionarAtaqueRival();
 }
 function ataqueElectricidad() {
-    ataqueJugador = 'ELECTRICIDAD'
+    ataqueJugador = 'PODER DE TIERRA'
     alert('Tu Dragon lanza '+ ataqueJugador +' a su oponente')
     seleccionarAtaqueRival();
 }
+
 
 function seleccionarAtaqueRival(){
     let ataqueRival = aleatorio(1,4)
 
     if(ataqueRival == 1){
-        ataqueOponente = 'FUEGO'
+        ataqueOponente = 'ALIENTO DE FUEGO'
         alert('El Dragon rival lanza '+ ataqueOponente +' a tu dragon')
         
 
     }else if(ataqueRival == 2){
-        ataqueOponente = 'AGUA'
+        ataqueOponente = 'ALIENTO DE HIELO'
         alert('El Dragon rival lanza '+ ataqueOponente +' a tu dragon')
         
     }
-    else if(ataqueRival == 2){
-        ataqueOponente = 'TIERRA'
+    else if(ataqueRival == 3){
+        ataqueOponente = 'RAFAGA DE VIENTO'
         alert('El Dragon rival lanza '+ ataqueOponente +' a tu dragon')
        
     }
-    else if(ataqueRival == 2){
-        ataqueOponente = 'ELECTRICIDAD'
+    else if(ataqueRival == 4){
+        ataqueOponente = 'PODER DE TIERRA'
         alert('El Dragon rival lanza '+ ataqueOponente +' a tu dragon')
     }
 
@@ -148,36 +191,6 @@ function seleccionarAtaqueRival(){
     crearMensaje();
     
 }
-
-function crearMensaje(){
-    let sectionMensajes = document.getElementById('mensajes')
-    let parrafo = document.createElement('p')
-    parrafo.innerHTML = 'Tu dragon ataco con ' +ataqueJugador +', Tu oponente ataco con ' +ataqueOponente + ' - ' +resultadoBatalla;
-
-    sectionMensajes.appendChild(parrafo)
-}
-
-function mensajeFinal(){
-    let sectionMensajeFinal = document.getElementById('mensaje-final')
-
-    let parrafoFinal =document.createElement('p')
-    parrafoFinal.innerHTML= resultadoFinal
-
-    sectionMensajeFinal.appendChild(parrafoFinal)
-
-    let botonFuego = document.getElementById('boton-fuego')
-    botonFuego.disabled = true
-    let botonAgua = document.getElementById('boton-agua')
-    botonAgua.disabled = true
-    let botonTierra = document.getElementById('boton-tierra')
-    botonTierra.disabled = true
-    let botonElectricidad = document.getElementById('boton-electricidad')
-    botonElectricidad.disabled = true
-
-    let selectionReinicio = document.getElementById('boton-reiniciar')
-    selectionReinicio.style.display = 'block'
-}
-
 function batallaDragones(){
     let spanVidasJugador = document.getElementById('vidas-jugador')
     let spanVidasRival = document.getElementById('vidas-rival')
@@ -185,22 +198,22 @@ function batallaDragones(){
     if(ataqueJugador == ataqueOponente) {
         resultadoBatalla = 'EMPATE'
         alert('EMPATE')
-    }else if( ataqueJugador == 'FUEGO' && ataqueOponente == 'ELECTRICIDAD'){
+    }else if( ataqueJugador == 'ALIENTO DE FUEGO' && ataqueOponente == 'ALIENTO DE HIELO'){
         resultadoBatalla = 'GANASTE'
         alert('GANASTE')
         vidasRival--
         spanVidasRival.innerHTML=vidasRival
-    }else if( ataqueJugador == 'AGUA' && ataqueOponente == 'FUEGO'){
+    }else if( ataqueJugador == 'ALIENTO DE HIELO' && ataqueOponente == 'RAFAGA DE VIENTO'){
         resultadoBatalla = 'GANASTE'
         alert('GANASTE')
         vidasRival--
         spanVidasRival.innerHTML=vidasRival
-    }else if( ataqueJugador == 'TIERRA' && ataqueOponente == 'ELECTRICIDAD'){
+    }else if( ataqueJugador == 'RAFAGA DE VIENTO' && ataqueOponente == 'PODER DE TIERRA'){
         resultadoBatalla = 'GANASTE'
         alert('GANASTE')
         vidasRival--
         spanVidasRival.innerHTML=vidasRival
-    }else if( ataqueJugador == 'ELECTRICIDAD' && ataqueOponente == 'AGUA'){
+    }else if( ataqueJugador == 'PODER DE TIERRA' && ataqueOponente == 'ALIENTO DE FUEGO'){
         resultadoBatalla = 'GANASTE'
         alert('GANASTE')
         vidasRival--
@@ -230,6 +243,43 @@ function revisarVidas(){
         mensajeFinal();
     }
 }
+function crearMensaje(){
+    let sectionMensajes = document.getElementById('resultado')
+    let ataqueDelJugador = document.getElementById('ataqueDelJugador')
+    let ataqueDelEnemigo = document.getElementById('ataqueDelEnemigo')
+
+    
+    let nuevoAtaqueDelJugador = document.createElement('p')
+    let nuevoAtaqueDelEnemigo = document.createElement('p')
+
+    sectionMensajes.innerHTML = resultadoBatalla
+    nuevoAtaqueDelJugador.innerHTML = ataqueJugador
+    nuevoAtaqueDelEnemigo.innerHTML = ataqueOponente
+
+    ataqueDelJugador.appendChild(nuevoAtaqueDelJugador)
+    ataqueDelEnemigo.appendChild(nuevoAtaqueDelEnemigo)
+}
+
+function mensajeFinal(){
+    let sectionMensajes = document.getElementById('resultado2')
+
+    sectionMensajes.innerHTML= resultadoFinal
+
+    let botonFuego = document.getElementById('boton-fuego')
+    botonFuego.disabled = true
+    let botonAgua = document.getElementById('boton-agua')
+    botonAgua.disabled = true
+    let botonTierra = document.getElementById('boton-tierra')
+    botonTierra.disabled = true
+    let botonElectricidad = document.getElementById('boton-electricidad')
+    botonElectricidad.disabled = true
+
+    let prueba = document.getElementById('resultado')
+    prueba.style.display = 'none'
+    let selectionReinicio = document.getElementById('boton-reiniciar')
+    selectionReinicio.style.display = 'block'
+}
+
 
 function reiniciarJuego(){
     location.reload()
